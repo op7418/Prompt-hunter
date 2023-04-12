cabd96be1e864ab8defe98a8821821ffbed003e8
op7418 <op.7418@163.com>
2023-04-13 00:54:08 +0800

    将输入页面ID改为输入页面链接

--- Prompt hunter/settings.js
+++ Prompt hunter/settings.js
@@ -1,23 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const settingsForm = document.getElementById("settingsForm");
-    const notionApiKeyInput = document.getElementById("notionApiKey");
-    const notionDatabaseIdInput = document.getElementById("notionDatabaseId");
+  const settingsForm = document.getElementById("settingsForm");
+  const notionApiKeyInput = document.getElementById("notionApiKey");
+  const notionPageLinkInput = document.getElementById("notionPageLink");
 
-    // Load saved settings
-    chrome.storage.sync.get(["notionApiKey", "notionDatabaseId"], (result) => {
-      notionApiKeyInput.value = result.notionApiKey || "";
-      notionDatabaseIdInput.value = result.notionDatabaseId || "";
-    });
-
-    // Save settings
-    settingsForm.addEventListener("submit", (event) => {
-      event.preventDefault();
-      
-      chrome.storage.sync.set({
-        "notionApiKey": notionApiKeyInput.value,
-        "notionDatabaseId": notionDatabaseIdInput.value
-      }, () => {
-        alert("Settings saved!");
-      });
-    });
+  // Load saved settings
+  chrome.storage.sync.get(["notionApiKey", "notionPageLink"], (result) => {
+    notionApiKeyInput.value = result.notionApiKey || "";
+    notionPageLinkInput.value = result.notionPageLink || "";
   });
+
+  // Save settings
+  settingsForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const notionPageLink = notionPageLinkInput.value;
+    const notionDatabaseId = extractDatabaseIdFromPageLink(notionPageLink);
+
+    chrome.storage.sync.set(
+      {
+        notionApiKey: notionApiKeyInput.value,
+        notionDatabaseId: notionDatabaseId,
+        notionPageLink: notionPageLink,
+      },
+      () => {
+        alert("Settings saved!");
+      }
+    );
+  });
+});
+
+function extractDatabaseIdFromPageLink(pageLink) {
+  const regex = /([a-f0-9]{32})/;
+  const match = pageLink.match(regex);
+
+  if (match) {
+    return match[0];
+  }
+  return "";
+}
